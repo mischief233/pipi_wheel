8d7558235315e78d81a4bfddf632f57ce7100937
mischeif233 <1258178933@qq.com>
2019-04-29 07:53:43 +0800

    去
    去

--- 特征工程/分箱特征.py
+++ 特征工程/分箱特征.py
@@ -7,4 +7,6 @@ Created on Thu Aug  9 17:50:45 2018
 import numpy as np
 import pandas as pd
 
-dataset.drop(dataset.columns[['无功功率控制状态','风机当前状态值','轮毂当前状态值','偏航状态值','偏航要求值']], axis=1,inplace=True)
+#分箱（qcut等量，cut等距，数字为份数）
+dataset['FareBin'] = pd.qcut(dataset['Fare'], 4)
+dataset['AgeBin'] = pd.cut(dataset['Age'].astype(int), 5)
